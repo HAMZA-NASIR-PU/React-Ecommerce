5e80c25e7290da9d582fa7159cb9fa9b445df6df
HAMZA-NASIR-PU <hamzanasir1111.hn@gmail.com>
2024-03-13 11:06:40 +0500

    add export default line in pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './Components/Navbar/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ShopCategory } from './Pages/ShopCategory';
 import { Product } from './Pages/Product';
+import { Cart } from './Pages/Cart';
 function App() {
   return (
     <div>
@@ -14,9 +15,10 @@ function App() {
           <Route path='/mens' element={<ShopCategory category="men" />} />
           <Route path='/womens' element={<ShopCategory category="women" />} />
           <Route path='/kids' element={<ShopCategory category="kid" />} />
-          <Route path='product' element={<Product}>
-
+          <Route path='product' element={<Product />}>
+            <Route path=':productId' element={<Product />} />
           </Route>
+          <Route path='/cart' element={<Cart />} />
         </Routes>
       </BrowserRouter>
 
