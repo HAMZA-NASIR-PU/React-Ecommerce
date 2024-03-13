fb4666afe2618bb7af158d03be0b1f53518699eb
HAMZA-NASIR-PU <hamzanasir1111.hn@gmail.com>
2024-03-13 10:59:47 +0500

    now creating pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ShopCategory } from './Pages/ShopCategory';
+import { Product } from './Pages/Product';
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Navbar />
 
+        <Routes>
+          <Route path='/' element={<Shop />} />
+          <Route path='/mens' element={<ShopCategory category="men" />} />
+          <Route path='/womens' element={<ShopCategory category="women" />} />
+          <Route path='/kids' element={<ShopCategory category="kid" />} />
+          <Route path='product' element={<Product}>
+
+          </Route>
+        </Routes>
       </BrowserRouter>
 
 
